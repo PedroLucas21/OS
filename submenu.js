8976341d1dbf9c1908861635223f10efa5594c15
PedroLucas21 <126469791+PedroLucas21@users.noreply.github.com>
2023-03-03 16:14:29 +0000

    Aula de TIC

---
+++ submenu.js
@@ -0,0 +1,6 @@
+let produtos = document.getElementById("menuprodutos");
+let produtosbutton = document.getElementById("produtos")
+
+produtosbutton.addEventListener('click', function (e) {
+    produtos.style
+});
