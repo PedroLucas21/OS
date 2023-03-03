811b810bdc9b5ebd9563835a1012c9c443b2d628
PedroLucas21 <126469791+PedroLucas21@users.noreply.github.com>
2023-03-03 22:31:50 +0000

    Zenbook

--- submenu.js
+++ submenu.js
@@ -1,6 +1,11 @@
-let produtos = document.getElementById("menuprodutos");
+let produtos = document.getElementById("menuprodutos")
 let produtosbutton = document.getElementById("produtos")
-
-produtosbutton.addEventListener('click', function (e) {
-    produtos.style
-});
+produtosbutton.addEventListener("mouseover", function (e) {
+	produtos.style.visibility = "visible";
+})
+produtos.addEventListener("mouseover", function (e) {
+	produtos.style.visibility = "visible";
+})
+document.addEventListener("mouseout", function (e) {
+	produtos.style.visibility = "hidden";
+})
